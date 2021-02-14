19d71a56240699bfeec8f709bad2308781c14477
ADITI NYAMAGOUDAR <78753898+aditi062002@users.noreply.github.com>
2021-02-15 00:34:31 +0530

    Update Area_Triangle.c

--- Area_Triangle.c
+++ Area_Triangle.c
@@ -5,7 +5,7 @@ float areaTriangle(float a, float b,float c)
 {
     float s,area;
     s=a+b+c/2.0;
-    area=sqrt(s*(s-a)(s-b)(s-c));
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
     return area;
 }
 int main()
